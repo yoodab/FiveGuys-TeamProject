656733aa71caa6dc5a72eff9423991669322587d
hsd9681 <hsd9681@naver.com>
2024-06-04 15:08:17 +0900

    init commit

---
+++ src/main/java/com/sparta/newspeed/NewsPeedApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.newspeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsPeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsPeedApplication.class, args);
+    }
+
+}
