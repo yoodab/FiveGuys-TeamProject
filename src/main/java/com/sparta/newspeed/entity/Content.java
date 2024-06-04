62b360eabedec219aee6a31ba7441356ebff4770
nenney <a01056499749@gmail.com>
2024-06-04 19:58:49 +0900

    add: content entity 추가

---
+++ src/main/java/com/sparta/newspeed/entity/Content.java
@@ -0,0 +1,37 @@
+package com.sparta.newspeed.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Content {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "content_id", unique = true, nullable = false)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL, targetEntity = User.class)
+    @JoinColumn(name = "user_id", updatable = false)
+    private user user;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "modified_at", nullable = false)
+    private Date modifiedAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+}
